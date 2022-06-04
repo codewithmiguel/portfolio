d56cf32ca24eb82ee8297c09760d80b9578e963e
codewithmiguel <codewithmiguel@gmail.com>
2022-06-04 15:04:04 -0300

    🎉feat: init project and component header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@mui/material'
+import { Header } from './components/header/index';
+
+function App() {
+
+  return (
+    <Box>
+      <Header />
+      Teste
+    </Box>
+  )
+}
+
+export default App
